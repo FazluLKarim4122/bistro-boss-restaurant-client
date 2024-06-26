f2dc0bf5797be96e9a9f064f7e84cfc816f91c2f
FazlulKarim4122 <md.fazlulkarim4122@gmail.com>
2024-06-26 14:29:45 +0600

    navbar home category component added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import Banner from '../Banner/Banner' 
+import Category from '../Category/Category';
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
+            <Category></Category>
             <h1>This is home</h1>
         </div>
     );
